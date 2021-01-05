fb8031ca72f6b82956e2d902308012871f5bdb87
edavi2020 <edavi2020@DEV-1-EEL-476.ad.campus-eni.fr>
2021-01-05 10:53:16 +0100

    classe ArticleVendu incomplete

---
+++ EniProjet/src/fr/eni/projetEni/bo/ArticleVendu.java
@@ -0,0 +1,25 @@
+
+public class ArticleVendu {
+
+	import java.util.Date;
+
+	public class ArticleVenduBo {
+
+		private int noArticle;
+		private String nomArticle;
+		private String description;
+		private Date dateDebutEncheres;
+		private Date dateFinEncheres;
+		private int miseAPrix;
+		private int prixVente;
+		private String etatVente;
+		private Utilisateur utilisateur;
+		private Categorie categorie;
+		private Retrait retrait;
+		
+		
+		
+		
+	}
+	
+}
