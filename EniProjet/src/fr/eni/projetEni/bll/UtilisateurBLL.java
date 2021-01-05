99b334599df1a4ec349905eed1e78c31a312dc2d
nicol <nicol@DESKTOP-CD7QT37.home>
2021-01-05 12:01:49 +0100

    Utilisateur BLL normalement terminé

--- EniProjet/src/fr/eni/projetEni/bll/UtilisateurBLL.java
+++ EniProjet/src/fr/eni/projetEni/bll/UtilisateurBLL.java
@@ -1,19 +1,11 @@
 package fr.eni.projetEni.bll;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 
 import fr.eni.projetEni.bo.UtilisateurBO;
-import fr.eni.projetEni.dal.ConnectionProvider;
-import fr.eni.projetEni.utils.MonLogger;
+import fr.eni.projetEni.dal.UtilisateurDAL;
 
 public class UtilisateurBLL {
-
-	private static Logger logger = MonLogger.getLogger("UtilisateurBLL");
 
     public static void insert(UtilisateurBO utilisateur )throws Exception {
     {
@@ -23,36 +15,60 @@ public class UtilisateurBLL {
     	{
     		throw new Exception("L'ensemble des informations doivent être renseignées");
     	}
-    }
-
-    public static List<UtilisateurBO>  get()
-    {
+    	
     	if(!utilisateur.getPseudo().matches("^[a-zA-Z0-9]*$"))
         {
-            throw new Exception("La plaque d'immatriculation doit être au format AAA111AAA");
+            throw new Exception("Le pseudo ne peut contenir que des lettres et des chiffres");
         }
-}
-
-    public static UtilisateurBO get(int id)
-    {
     	
+    	UtilisateurDAL.insert(utilisateur);
     }
-    public static UtilisateurBO getPseudo(String pseudo)
-    {
-    	
-    }
-    
-    public static UtilisateurBO getCourriel(String email)
-    {
-    
-    }
-    public static void update(UtilisateurBO utilisateur)
-    {
-       
-    }
+  }
 
-    public static void delete(int id)
+    public static List<UtilisateurBO> get()throws Exception {
     {
-       
+    	return UtilisateurDAL.get();
+    }
+   }
+
+    public static UtilisateurBO get(int id)throws Exception {
+    {
+    	return UtilisateurDAL.get(id);
+    }
+   }
+    
+    public static UtilisateurBO getPseudo(String pseudo) throws Exception {
+    {
+    	return UtilisateurDAL.getPseudo(pseudo);
+    }
+   }
+    public static UtilisateurBO getCourriel(String email)throws Exception {
+    {
+    	return UtilisateurDAL.getCourriel(email);
+    }
+   }
+    public static void update(UtilisateurBO utilisateur) throws Exception{
+    {
+    	if(utilisateur.getPseudo().trim().equals("")||utilisateur.getNom().trim().equals("")||utilisateur.getPrenom().trim().equals("")||
+    			utilisateur.getEmail().trim().equals("")||utilisateur.getTelephone().trim().equals("")||utilisateur.getRue().trim().equals("")|| 
+    			utilisateur.getCodePostal().trim().equals("")||utilisateur.getVille().trim().equals("")||utilisateur.getPassword().trim().equals(""))
+    	{
+    		throw new Exception("L'ensemble des informations doivent être renseignées");
+    	}
+    	
+    	if(!utilisateur.getPseudo().matches("^[a-zA-Z0-9]*$"))
+        {
+            throw new Exception("Le pseudo ne peut contenir que des lettres et des chiffres");
+        }
+    	
+    	UtilisateurDAL.update(utilisateur);
+    }
+    }
+    public static void delete(int id)throws Exception {
+        if(UtilisateurDAL.get(id) ==null)
+        {
+            throw new Exception("Il semblerait que cet utilisateur n'existe pas ou ait déjà été supprimé de la base de données.");
+        }
+        UtilisateurDAL.delete(id);
     }
 }
