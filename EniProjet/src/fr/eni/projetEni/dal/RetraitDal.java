68d572bf906934cf39fb7fc757068052f7185a5b
colle <colle@LAPTOP-Q80I611U>
2021-01-05 13:23:43 +0100

    ddd

--- EniProjet/src/fr/eni/projetEni/dal/RetraitDal.java
+++ EniProjet/src/fr/eni/projetEni/dal/RetraitDal.java
@@ -2,4 +2,9 @@ package fr.eni.projetEni.dal;
 
 public class RetraitDal {
 
+	private static final String INSERT = "INSERT INTO RETRAITS VALUES (?,?,?)";
+    private static final String GET_BY_ID = "SELECT * FROM RETRAITS WHERE no_categorie = ?";
+    private static final String GET_ALL = "SELECT * FROM RETRAITS";
+    private static final String UPDATE = "UPDATE RETRAITS SET nom = ?, vitesseMax = ? WHERE no_categorie = ?";
+    private static final String DELETE = "DELETE RETRAITS WHERE no_categorie = ?";
 }
