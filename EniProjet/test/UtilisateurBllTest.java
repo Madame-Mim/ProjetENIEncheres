4ec68701c9c9851f7abd127d688e1cb24c52cf17
nicol <nicol@DESKTOP-CD7QT37.home>
2021-01-05 13:38:16 +0100

    modif test utilisateur

--- EniProjet/test/UtilisateurBllTest.java
+++ EniProjet/test/UtilisateurBllTest.java
@@ -6,19 +6,124 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import fr.eni.projetEni.bo.UtilisateurBo;
+import fr.eni.projetEni.bll.UtilisateurBll;
+
 class UtilisateurBllTest {
 
-	@BeforeEach
-	void setUp() throws Exception {
-	}
+	private static Logger logger = MonLogger.getLogger("PersonneBllTest");
 
-	@AfterEach
-	void tearDown() throws Exception {
-	}
+    @BeforeEach
+    void setUp() {
+        logger.info("setUp");
+    }
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    @AfterEach
+    void tearDown() {
+        List<UtilisateurBo> utilisateur = UtilisateurBll.get();
+        for (utilisateur item : utilisateurs) {
+            try {
 
+            	UtilisateurBll.delete(item.getId());
+            } catch (Exception e) {
+                fail("erreur : " + e.getMessage());
+            }
+        }
+    }
+
+    @Test
+    void insert() {
+    	UtilisateurBo utilisateur = new UtilisateurBo();
+    	utilisateur.setNom("Picasso");
+    	utilisateur.setPrenom("Pablo");
+        try {
+        	UtilisateurBll.insert(utilisateur);
+        } catch (Exception e) {
+            fail("erreur : " + e.getMessage());
+        }
+
+        List<UtilisateurBo> utilisateur = UtilisateurBll.get();
+        utilisateur dernierePersonne = utilisateurs.get(utilisateurs.size() - 1);
+
+        assertEquals(dernierePersonne.getNom(), utilisateur.getNom());
+        assertEquals(dernierePersonne.getPrenom(), utilisateur.getPrenom());
+
+    }
+
+    @Test
+    void get() {
+    	utilisateur p1 = new Utilisateur("Picassa", "Paola");
+    	utilisateur p2 = new Utilisateur("Picassi", "Paoli");
+    	utilisateur p3 = new Utilisateur("Picasse", "Paule");
+        try {
+        	UtilisateurBll.insert(p1);
+        	UtilisateurBll.insert(p2);
+        	UtilisateurBll.insert(p3);
+        } catch (Exception e) {
+            fail("erreur : " + e.getMessage());
+        }
+
+
+        List<Utilisateur> utilisateurs = UtilisateurBll.get();
+        assertEquals(3, utilisateurs.size());
+
+
+    }
+
+    @Test
+    void testGet() throws Exception {
+        Personne p1 = new Personne("Picassa", "Paola");
+        Personne p2 = new Personne("Picassi", "Paoli");
+        Personne p3 = new Personne("Picasse", "Paule");
+
+            UtilisateurBll.insert(p1);
+            UtilisateurBll.insert(p2);
+            UtilisateurBll.insert(p3);
+
+        List<Personne> utilisateurs = UtilisateurBll.get();
+        Personne personnearecup =utilisateurs.get(2);
+
+        Personne personnerecuperee = UtilisateurBll.get(personnearecup.getId());
+        assertEquals(personnearecup.getNom(), personnerecuperee.getNom());
+        assertEquals(personnearecup.getPrenom(), personnerecuperee.getPrenom());
+        assertEquals(personnearecup.getId(), personnerecuperee.getId());
+    }
+
+    @Test
+    void update() throws Exception {
+
+        Personne utilisateur = new Personne();
+        utilisateur.setNom("Picasso");
+        utilisateur.setPrenom("Pablo");
+
+        UtilisateurBll.insert(utilisateur);
+        List<Personne> utilisateurs = UtilisateurBll.get();
+        Personne personneamodifier = utilisateurs.get(0);
+
+        personneamodifier.setNom("Picassa");
+        personneamodifier.setPrenom("Paola");
+
+        UtilisateurBll.update(personneamodifier);
+        Personne personnemodifiee = UtilisateurBll.get(personneamodifier.getId());
+
+        assertEquals(personnemodifiee.getNom(), "Picassa");
+        assertEquals(personnemodifiee.getPrenom(), "Paola");
+    }
+
+
+    @Test
+    void delete() throws Exception {
+        Personne utilisateur = new Personne();
+        utilisateur.setNom("Picasso");
+        utilisateur.setPrenom("Pablo");
+
+        UtilisateurBll.insert(utilisateur);
+        List<Personne> utilisateurs = UtilisateurBll.get();
+
+        for (Personne item : utilisateurs) {
+            UtilisateurBll.delete(item.getId());
+        }
+        utilisateurs = UtilisateurBll.get();
+        assertEquals(0, utilisateurs.size());
+    }
 }
