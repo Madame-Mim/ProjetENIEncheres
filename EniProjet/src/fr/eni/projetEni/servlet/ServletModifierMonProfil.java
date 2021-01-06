ed4d7762dc9df7d64663216f254103bc392bb544
edavi2020 <edavi2020@DEV-1-EEL-476.ad.campus-eni.fr>
2021-01-06 16:10:40 +0100

    re

--- EniProjet/src/fr/eni/projetEni/servlet/ServletModifierMonProfil.java
+++ EniProjet/src/fr/eni/projetEni/servlet/ServletModifierMonProfil.java
@@ -20,7 +20,7 @@ public class ServletModifierMonProfil extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
         this.getServletContext().getRequestDispatcher( "/WEB-INF/Encheres/Utilisateur/modifierMonProfil.jsp" ).forward( request, response );
-	}
+	} 
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
