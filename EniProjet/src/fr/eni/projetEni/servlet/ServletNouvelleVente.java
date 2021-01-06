a9772ee36145287f60285ba16ff46e3f98cd1eb4
colle <colle@LAPTOP-Q80I611U>
2021-01-06 13:40:21 +0100

    Maj

---
+++ EniProjet/src/fr/eni/projetEni/servlet/ServletNouvelleVente.java
@@ -0,0 +1,35 @@
+package fr.eni.projetEni.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletNouvelleVente
+ */
+@WebServlet("/ServletNouvelleVente")
+public class ServletNouvelleVente extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("ServletNouvelleVente - doGet");
+		RequestDispatcher rs = request.getRequestDispatcher("/WEB-INF/nouvelleVente.jsp");
+		rs.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("ServletNouvelleVente - doPost");
+	}
+
+}
