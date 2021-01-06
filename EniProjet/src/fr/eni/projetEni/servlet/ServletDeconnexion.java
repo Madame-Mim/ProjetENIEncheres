3c9856bdc1c0c504d0b60a6a921d5f723cf3a6ee
edavi2020 <edavi2020@DEV-1-EEL-476.ad.campus-eni.fr>
2021-01-06 14:53:56 +0100

    Comme ça, c'est fait...

---
+++ EniProjet/src/fr/eni/projetEni/servlet/ServletDeconnexion.java
@@ -0,0 +1,28 @@
+package fr.eni.projetEni.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class ServletDeconnexion
+ */
+@WebServlet("/ServletDeconnexion")
+public class ServletDeconnexion extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	public static final String URL_REDIRECTION = "http://localhost:8080/EniProjet/ServletAccueil";
+
+    public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+        /* Récupération et destruction de la session en cours */
+        HttpSession session = request.getSession();
+        session.invalidate();
+
+        /* Redirection vers la page d'accueil */
+        response.sendRedirect( URL_REDIRECTION );
+    }
+}
