7e53af9d4a3ec656225090643803ae0b87421be0
nicol <nicol@DESKTOP-CD7QT37.home>
2021-01-06 17:24:52 +0100

    modif servlet encherir

--- EniProjet/src/fr/eni/projetEni/servlet/ServletEncherir.java
+++ EniProjet/src/fr/eni/projetEni/servlet/ServletEncherir.java
@@ -18,6 +18,7 @@ public class ServletEncherir extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 	}
 
 	/**
