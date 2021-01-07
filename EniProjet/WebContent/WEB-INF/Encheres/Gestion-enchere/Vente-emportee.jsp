5cca8262964200cb5edf714ea12920e2cf878b6d
nicol <nicol@DESKTOP-CD7QT37.home>
2021-01-07 11:42:51 +0100

    ajout vente emportee

--- EniProjet/WebContent/WEB-INF/Encheres/Gestion-enchere/Vente-emportee.jsp
+++ EniProjet/WebContent/WEB-INF/Encheres/Gestion-enchere/Vente-emportee.jsp
@@ -13,7 +13,7 @@
 	</header>
 	
 	<section>
-		<h1>Détail vente</h1>
+		<h1>${article.utilisateur} a remporté l'enchère</h1>
 		
 		${article.nom}
 		<br>
@@ -31,9 +31,10 @@
 		<br>
 		 
 		<form action="<%=request.getContextPath()%>/confirmationRetrait" method="post">
-			
+			<input type="hidden" name="credit" value="${article.offre}">
 			<input type="submit" value="retrait effectué">
 		</form>
+		<p><%=request.getAttribute("affichageListeArticle") %></p>
 		
 	</section>
 	
---
+++ EniProjet/src/fr/eni/projetEni/servlet/ServletVenteEmportee.java
@@ -0,0 +1,60 @@
+package fr.eni.projetEni.servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import fr.eni.projetEni.bll.ArticleVenduBll;
+import fr.eni.projetEni.bll.UtilisateurBll;
+import fr.eni.projetEni.bo.ArticleVenduBo;
+import fr.eni.projetEni.bo.UtilisateurBo;
+
+/**
+ * Servlet implementation class ServletVenteEmportee
+ */
+@WebServlet("/confirmationRetrait")
+public class ServletVenteEmportee extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		List<ArticleVenduBo> listeArticles;
+		try {
+			listeArticles = ArticleVenduBll.getAll();
+			for(ArticleVenduBo article : listeArticles)
+	        {
+	            //"affichageListeArticle" doit apparaître dans la jsp pour afficher la liste comme ceci
+	            //<p><%=request.getAttribute("affichageListeArticle") %> </p>
+	            request.setAttribute("affichageListeArticle", listeArticles);
+	            System.out.println(article);
+	        }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        
+        //le forward envoi l'affichage à la jsp
+        RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/Encheres/Gestion-enchere/Vente-emportee.jsp");
+        rd.forward(request, response);
+		}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		UtilisateurBo utilisateurACrediter;
+		utilisateurACrediter = new UtilisateurBo();
+		utilisateurACrediter.setCredit(request.getParameter("credit"));
+		
+		request.setAttribute("utilisateur", utilisateurACrediter);
+		UtilisateurBll.update(utilisateurACrediter);
+	}
+
+}
