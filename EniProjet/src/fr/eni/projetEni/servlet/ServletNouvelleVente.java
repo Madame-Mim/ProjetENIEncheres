862f36c9f85d2d2020dc18e854112c15287b0b33
colle <colle@LAPTOP-Q80I611U>
2021-01-07 13:12:28 +0100

    gg

--- EniProjet/src/fr/eni/projetEni/servlet/ServletNouvelleVente.java
+++ EniProjet/src/fr/eni/projetEni/servlet/ServletNouvelleVente.java
@@ -28,7 +28,7 @@ import fr.eni.projetEni.dal.ArticleVenduDal;
 public class ServletNouvelleVente extends HttpServlet {
 	private static final long serialVersionUID = 1L;
    
-	/**
+	/** 
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
