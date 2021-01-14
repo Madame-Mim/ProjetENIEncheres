6192d0cf2da563917528a05be766c1bd07ef3ab8
nicol <nicol@DESKTOP-CD7QT37.home>
2021-01-14 09:20:02 +0100

    maj futile

--- EniProjet/src/fr/eni/projetEni/servlet/ServletVenteEnCours.java
+++ EniProjet/src/fr/eni/projetEni/servlet/ServletVenteEnCours.java
@@ -26,7 +26,7 @@ import fr.eni.projetEni.bo.UtilisateurBo;
 @WebServlet("/VenteEnCours")
 public class ServletVenteEnCours extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       //
+       
    	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
