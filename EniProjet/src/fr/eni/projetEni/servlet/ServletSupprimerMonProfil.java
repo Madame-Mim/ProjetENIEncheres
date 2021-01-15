e1883fb27b70a4ebcebc2eeeb366044856285d53
edavi2020 <edavi2020@DEV-1-EEL-476.ad.campus-eni.fr>
2021-01-15 10:28:16 +0100

    supprimer

--- EniProjet/src/fr/eni/projetEni/servlet/ServletSupprimerMonProfil.java
+++ EniProjet/src/fr/eni/projetEni/servlet/ServletSupprimerMonProfil.java
@@ -12,17 +12,18 @@ import fr.eni.projetEni.bll.UtilisateurBll;
 import fr.eni.projetEni.bo.UtilisateurBo;
 
 /**
+ * @author edavi2020
  * Servlet implementation class ServletSupprimerMonProfil
  */
 @WebServlet("/ServletSupprimerMonProfil")
 public class ServletSupprimerMonProfil extends HttpServlet {
-	private static final long serialVersionUID = 1L;
 	
+	private static final long serialVersionUID = 1L;
 	private UtilisateurBll utilisateurCree = new UtilisateurBll();
 
-       
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+		//
 		HttpSession session = request.getSession();
 		session.getAttribute("session");
 		int id= Integer.parseInt(session.getAttribute("session").toString());
@@ -31,7 +32,6 @@ public class ServletSupprimerMonProfil extends HttpServlet {
 			UtilisateurBo utilisateur = UtilisateurBll.get(id);
 			request.setAttribute("utilisateur", utilisateur);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
@@ -39,11 +39,10 @@ public class ServletSupprimerMonProfil extends HttpServlet {
 		try {
 			utilisateurCree.delete(id);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-		//session.invalidate();
+		session.invalidate();
 		
         this.getServletContext().getRequestDispatcher( "/Accueil" ).forward( request, response );
 	} 
