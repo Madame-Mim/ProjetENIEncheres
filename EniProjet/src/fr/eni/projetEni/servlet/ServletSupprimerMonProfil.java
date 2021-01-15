53649641654edb8fa00fb77a82ff0bff5420f608
nicol <nicol@DESKTOP-CD7QT37.home>
2021-01-15 12:34:03 +0100

    suppression profil qui marche

--- EniProjet/src/fr/eni/projetEni/servlet/ServletSupprimerMonProfil.java
+++ EniProjet/src/fr/eni/projetEni/servlet/ServletSupprimerMonProfil.java
@@ -1,6 +1,9 @@
 package fr.eni.projetEni.servlet;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,7 +11,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import fr.eni.projetEni.bll.ArticleVenduBll;
+import fr.eni.projetEni.bll.EnchereBll;
 import fr.eni.projetEni.bll.UtilisateurBll;
+import fr.eni.projetEni.bo.ArticleVenduBo;
+import fr.eni.projetEni.bo.EnchereBo;
 import fr.eni.projetEni.bo.UtilisateurBo;
 
 /**
@@ -27,24 +34,64 @@ public class ServletSupprimerMonProfil extends HttpServlet {
 		session.getAttribute("session");
 		int id= Integer.parseInt(session.getAttribute("session").toString());
 	
-		try {
+		EnchereBll enchereAmodifié= new EnchereBll();
+		UtilisateurBo utilisateurAmodifie;
+		try
+		{
+			utilisateurAmodifie = UtilisateurBll.get(id);
+			List<EnchereBo> listeEnchere = EnchereBll.getbyutilisateur(id);
+			for (int i = 0; i < listeEnchere.size(); i++) 
+			{
+				EnchereBo enchere = new EnchereBo();
+				enchere.setNoUtilisateur(utilisateurAmodifie);
+				enchereAmodifié.updateAll(enchere);
+			}
+		
+		}
+		catch (Exception e1) 
+		{
+			e1.printStackTrace();
+		}
+		//maj des articles vers "utilisateur supprimé"
+
+		ArticleVenduBll articleAmodifié= new ArticleVenduBll();
+		UtilisateurBo utilisateurArticleAmodifie;
+		
+		try
+		{
+			utilisateurArticleAmodifie = UtilisateurBll.get(id);
+
+			List<ArticleVenduBo> listeArticle = (List<ArticleVenduBo>) ArticleVenduBll.getByIdUtilisateur(id);
+
+			for (int i = 0; i < listeArticle.size(); i++) 
+			{
+				ArticleVenduBo article = new ArticleVenduBo();
+				article.setUtilisateur(utilisateurArticleAmodifie);
+				articleAmodifié.updateAll(article);
+			}
+		
+		}
+		catch (Exception e1) 
+		{
+			e1.printStackTrace();
+		}
+
+					
+		UtilisateurBll utilisateurASupprimer = new UtilisateurBll();
+		try 
+		{
 			UtilisateurBo utilisateur = UtilisateurBll.get(id);
-			request.setAttribute("utilisateur", utilisateur);
-		} catch (Exception e) {
+			utilisateurASupprimer.delete(id);
+			
+			session.invalidate();
+			
+			response.sendRedirect( "http://localhost:8080/EniProjet/Accueil" );
+		} 
+		catch (Exception e) 
+		{
 			e.printStackTrace();
 		}
-		
-		
-		try {
-			utilisateurCree.delete(id);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		session.invalidate();
-		
-		response.sendRedirect( "/WEB-INF/Encheres/accueilvisiteur.jsp" );
-	} 
+	}
 
 
 	/**
